b7da810001a26b8d6972cfa2fb88a001be2f4ea3
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-06-25 00:34:57 +0300

    Create ibutton_component.h

---
+++ custom_components/ibutton/ibutton_component.h
@@ -0,0 +1,23 @@
+#ifndef ESPHOME_IBUTTON_COMPONENT_H_
+#define ESPHOME_IBUTTON_COMPONENT_H_
+
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/gpio/base.h"
+#include "esphome/components/onewire/one_wire.h"
+
+namespace esphome {
+
+class IButtonComponent : public Component, public BinarySensor {
+ public:
+    void set_one_wire_bus(const one_wire::OneWireBus *bus) { bus_ = bus; };
+    void set_read_rom_command(uint8_t command) { read_rom_command_ = command; };
+    void log_serial_number() const;
+
+ protected:
+    const one_wire::OneWireBus* bus_{nullptr};  // Однонаправленная шина
+    uint8_t read_rom_command_;                 // Команда для чтения серийника
+};
+
+}  // namespace esphome
+
+#endif  // ESPHOME_IBUTTON_COMPONENT_H_
