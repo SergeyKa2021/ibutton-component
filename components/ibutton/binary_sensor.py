284a5ecbe7cdebca39c847c0bbca062216e31d27
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-06-25 19:45:36 +0300

    Update binary_sensor.py

--- components/ibutton/binary_sensor.py
+++ components/ibutton/binary_sensor.py
@@ -1,16 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL, DEVICE_CLASS_CONNECTIVITY
-from esphome.core.entity_helpers import inherit_property_from
+from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
+#from esphome.core.entity_helpers import inherit_property_from
 
 #from .const import CONF_ONE_WIRE_BUS, CONF_READ_ROM_COMMAND, DEFAULT_IBUTTON_READ_ROM_COMMAND
 
-CODEOWNERS = ["@SergeyKa2021"]  # Измените на ваше имя пользователя
-DEPENDENCIES = ["one_wire"]
-MULTI_CONF = True
+CODEOWNERS = ["@SergeyKa2021"] 
+#DEPENDENCIES = ["one_wire"]
+#MULTI_CONF = True
 
-CONF_ONE_WIRE_BUS = 0
+#CONF_ONE_WIRE_BUS = 0
 CONF_READ_ROM_COMMAND = 0x33
 DEFAULT_IBUTTON_READ_ROM_COMMAND = 0x33
 
@@ -20,22 +20,31 @@ ibutton_ns = cg.esphome_ns.namespace("ibutton")
 IButtonComponent = ibutton_ns.class_(
     "IButtonComponent", 
     binary_sensor.BinarySensor, 
-#    cg.Component
+    cg.Component
 )
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(IButtonComponent),
+CONFIG_SCHEMA = (
+    binary_sensor.binary_sensor_schema(EmptyBinarySensor)
+    .extend(
+        {
+            cv.GenerateID(): cv.declare_id(EmptyBinarySensor),
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+#CONFIG_SCHEMA = cv.Schema(
+#    {
+#        cv.GenerateID(): cv.declare_id(IButtonComponent),
 #        cv.Required(CONF_ONE_WIRE_BUS): cv.use_id(cg.Component),
 #        cv.Optional(CONF_READ_ROM_COMMAND, default=DEFAULT_IBUTTON_READ_ROM_COMMAND): cv.hex_uint8_t,
-        cv.Optional(CONF_UPDATE_INTERVAL, default='60s'): cv.update_interval,
-    }
-).extend(cv.COMPONENT_SCHEMA)
+#        cv.Optional(CONF_UPDATE_INTERVAL, default='60s'): cv.update_interval,
+#    }
+#).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-#    var = cg.new_Pvariable(config[CONF_ID])
+    var = await binary_sensor.new_binary_sensor(config)
     await cg.register_component(var, config)
-    await binary_sensor.register_binary_sensor(var, config)
 
 #    one_wire_bus = await cg.get_variable(config[CONF_ONE_WIRE_BUS])
 #    cg.add(var.set_one_wire_bus(one_wire_bus))
