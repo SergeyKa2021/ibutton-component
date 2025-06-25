bb47830f451da90aa2c128da273bf13df79958f3
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-06-25 19:26:32 +0300

    Update ibutton.h

--- components/ibutton/ibutton.h
+++ components/ibutton/ibutton.h
@@ -1,6 +1,3 @@
-#ifndef ESPHOME_IBUTTON_COMPONENT_H_
-#define ESPHOME_IBUTTON_COMPONENT_H_
-
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/gpio/base.h"
 #include "esphome/components/onewire/one_wire.h"
@@ -9,17 +6,15 @@ namespace esphome {
 namespace ibutton {
 
 class IButtonComponent : public Component, public BinarySensor {
- public:
-    void set_one_wire_bus(const one_wire::OneWireBus *bus) { bus_ = bus; };
-    void set_read_rom_command(uint8_t command) { read_rom_command_ = command; };
-    void log_serial_number() const;
+// public:
+//    void set_one_wire_bus(const one_wire::OneWireBus *bus) { bus_ = bus; };
+//    void set_read_rom_command(uint8_t command) { read_rom_command_ = command; };
+//    void log_serial_number() const;
 
- protected:
-    const one_wire::OneWireBus* bus_{nullptr};  // Один провод для коммуникации
-    uint8_t read_rom_command_;                 // Команда для чтения серийного номера
+// protected:
+//    const one_wire::OneWireBus* bus_{nullptr};  // Один провод для коммуникации
+//    uint8_t read_rom_command_;                 // Команда для чтения серийного номера
 };
 
 }  // namespace ibutton
 }  // namespace esphome
-
-#endif  // ESPHOME_IBUTTON_COMPONENT_H_
