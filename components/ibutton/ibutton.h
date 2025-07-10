9ff1fa05762a4bd10eb333bb3e8102ce13e88281
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-07-10 23:09:07 +0300

    Update ibutton.h

--- components/ibutton/ibutton.h
+++ components/ibutton/ibutton.h
@@ -1,6 +1,7 @@
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/gpio/one_wire/gpio_one_wire.h"
 #include "esphome/components/one_wire/one_wire.h"
+#include "esphome/core/component.h"
 
 namespace esphome {
 namespace ibutton {
