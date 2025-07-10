c6c17fa4fa8adf2bebebb617d12ca8a77510594e
SergeyKa2021 <92687528+SergeyKa2021@users.noreply.github.com>
2025-07-10 23:05:44 +0300

    Update ibutton.h

--- components/ibutton/ibutton.h
+++ components/ibutton/ibutton.h
@@ -6,7 +6,7 @@ namespace esphome {
 namespace ibutton {
 
 class IButtonComponent : public Component, public BinarySensor {
-// public:
+ public:
 //    void set_one_wire_bus(const one_wire::OneWireBus *bus) { bus_ = bus; };
 //    void set_read_rom_command(uint8_t command) { read_rom_command_ = command; };
 //    void log_serial_number() const;
